d813a7e028bbd4986f5e09b782679fe9331ed439
Hummel83 <chrisschulz@gmx.net>
2018-01-20 20:24:23 +0100

    rename Projects

---
+++ BusinessObjectLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjectLib
+{
+    public class Class1
+    {
+    }
+}
