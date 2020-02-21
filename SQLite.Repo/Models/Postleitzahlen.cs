15f878b2a236a9efe6072aaf404a5df02e616b82
Christian Schulz <chrisschulz@gmx.net>
2020-02-21 17:14:44 +0100

    add database functions

--- SQLite.Repo/Models/Postleitzahlen.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SQLite.Repo.Models
-{
-    public class Postleitzahlen 
-    {
-        public int AdressId { get; set; }
-        public string Postleitzahl { get; set; }
-    }
-}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+
+using Xunit;
+
+namespace UnitTests
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
