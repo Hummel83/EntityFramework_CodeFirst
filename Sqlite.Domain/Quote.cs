16cdf9b3e8ee8ff31c39d94f4379385ffcff2ddf
Christian Schulz <chrisschulz@gmx.net>
2020-03-22 19:56:32 +0100

    finish chapter 3

--- Sqlite.Domain/Quote.cs
+++ Sqlite.Domain/Quote.cs
@@ -2,7 +2,7 @@
 {
     public class Quote
     {
-        public int Id { get; set; }
+        public int QuoteId { get; set; }
         public string Text { get; set; }
         public Samurai Samurai { get; set; }
         public int SamuraiId { get; set; }
